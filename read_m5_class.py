af05aa47de4d48480e00312a5485b50204d286cf
chibaf <chibaf@mac.com>
2022-05-30 23:46:22 +0900

    Add files via upload

--- read_m5_class.py
+++ read_m5_class.py
@@ -3,9 +3,19 @@ class m5logger:
   def read_logger(self,ser):
     import serial
     line = ser.readline()
-    line2=line.strip().decode('utf-8')
+    while True:
+      try:
+        line2=line.strip().decode('utf-8')
+      except UnicodeDecodeError:
+        line = ser.readline()
+        continue
+      break
     data = [str(val) for val in line2.split(",")]
     data1=[]
     for i in range(0,10):
-      data1.append(float(data[i+2]))
+      try:
+        data1.append(float(data[i+2]))
+      except IndexError:
+        data1=[0.0]*10
+        break
     return data1
